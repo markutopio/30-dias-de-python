e3c84f6126f5cd5dee4c5f335e672129a8b4cccd
Marcos <marcodelarosaaaa@gmail.com>
2022-10-24 12:09:44 +0200

    w

---
+++ day1/helloword.py
@@ -0,0 +1,37 @@
+1.2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+1.3
+
+print("Marco")
+print("Palma")
+print("Spain")
+print("I am enjoying 30 days of python")
+
+1.4
+
+print(type(10))          
+print(type(9.8))        
+print(type(4 - 4j))      
+print(type(['Asabeneh', 'Python', 'Finland']))  
+print(type('Marco'))   
+print(type('Palma')) 
+print(type('Spain'))
+
+3
+print('integer is 1, float is 1.1, complex is 7 - 4j')
+list=("si","no")
+mytuple=("BMW","HONDA","SKODA")
+print(mytuple)
+str("si")
+print("boolean is 'true'")
+
+
+
+
