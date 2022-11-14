701d1150f6bc8568f0c037b166b248006b4b5842
Marcos <marcodelarosaaaa@gmail.com>
2022-11-14 12:35:49 +0100

    cambiosss

--- day3/day3.py
+++ day3/day3.py
@@ -4,11 +4,11 @@ print(age)
 
 -2
 height = 1.72
-print(height)
+print('Floating point number, height', 1.72)
 
 -3
 complex = 6 + 4j
-print(complex)
+print('complex number: ', 6 + 4j)
 
 -4
 b = 20
@@ -23,3 +23,17 @@ sidec = 3
 perimeter = sidea+sideb+sidec
 print(perimeter)
 
+-6
+width = 3
+lenght = 4
+area_rectangulo = width * lenght
+perimeter_rectangulo = 2 * (width+lenght)
+print(area_rectangulo, perimeter_rectangulo)
+
+-7
+r1 = 4
+r2 = 3
+pi = 3.14
+radius_circle = pi * r1 * r2
+circle_circumference = 2 * pi * r1
+print(radius_circle, circle_circumference)
