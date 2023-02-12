bee17a8496ebf5ee484f5efc551e66e39ea7d434
marco <marcodelarosaaaa@gmail.com>
2023-02-12 13:19:03 +0100

    todo

---
+++ day11/day11.py
@@ -0,0 +1,56 @@
+
+-1
+def add_two_numbers(a,b):
+    return a + b
+
+-2
+def area_of_a_circle(r):
+    return 3.1416 * r**2
+    
+-3
+def add_all_nums(aas):
+    for i in aas:
+        aas = aas + 1
+        print(aas)
+
+-4
+def from_celsius_to_fahrenheit(celsius):
+    return(celsius * 9 / 5) + 32
+
+-5
+def check_season(mes):
+    if mes in ["march", "april", "may"]:
+        return("Spring")
+    
+    elif mes in ["june", "july", "august"]:
+        return("Summer")
+
+    elif mes in ["septiembre", "october", "november"]:
+        return("Autumn")
+
+    elif mes in ["december", "january", "february"]:
+        return("Winter")
+
+
+-6
+def calculate_slope(x1, x2):
+    return x2 + x1
+
+-7
+def solve_quadratic_eqn(x1,x2,x3):
+    solve = x2 * x2 -4 * x1 * x3 / 2 * x1
+    return solve
+
+-8
+def print_list(lss):
+    lss = []
+    for i in lss:
+        print(i)
+
+-9
+def reverse_list(lia):
+    return lia[::-1]
+
+
+-10
+
