d943752f1b0b9ce2b01323365b0b9a6c02e3adc6
markutopio <101260773+markutopio@users.noreply.github.com>
2023-02-12 12:20:18 +0100

    Update day10.py

--- day10/day10.py
+++ day10/day10.py
@@ -22,3 +22,46 @@ while new_count >= 0:
     new_count  = new_count - 1
 
 
+-3
+hags = "#"
+
+for i in range(1,9):
+    i = i * hags
+    print(i)
+
+-4
+hags_2 = "#"
+
+for i in range(1,9):
+    for r in range(1,9):
+        print(hags_2, end="")
+
+
+-5
+for i in range(0,11):
+    print(i, "x", i, "=", i*i)
+
+
+-6
+list = ["Python", "Numpy", "Pandas", "Diango", "Flask"]
+
+for contents in list:
+    print(contents)
+
+-7
+for i in range(0,101):
+    print(i)
+
+
+-8
+for num in range(0,5005):
+    num = num + 1
+    print("All is ", num)
+
+
+-9
+listt = ["Finland, Iceland, Ireland, the Netherlands, Poland, Switzerland, Thailand, New Zealand"]
+
+for country in listt:
+    if "land" in country:
+        print(country)
